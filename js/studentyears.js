c930d4a81f3a594da7994308302202994c503bbe
cooljiansir <cooljiansir@gmail.com>
2015-03-23 14:31:48 +0800

    首页下拉

--- js/studentyears.js
+++ js/studentyears.js
@@ -1,3 +1,26 @@
+$(".gdropdown").click(function(){
+	var isopen = $(this).next().data("isopen");
+	console.log("thisnext"+isopen);
+	if(typeof(isopen) == "undefined"||isopen == false){
+		$(this).next().slideDown(200);
+		$(this).next().data("isopen",true);
+		$(this).children("span").removeClass("glyphicon-menu-down");
+		$(this).children("span").addClass("glyphicon-menu-up");
+	}
+	else{
+		$(this).next().slideUp(200);
+		$(this).next().data("isopen",false);
+		$(this).children("span").removeClass("glyphicon-menu-up");
+		$(this).children("span").addClass("glyphicon-menu-down");
+	}
+});
+/*$(".gdropdown").mouseleave(function(){
+	$(this).next().slideUp(200);
+});
+$(".gdropdown-menu").mouseleave(function(){
+	$(this).slideUp(200);
+});*/
+
 $("#loginBut").click(function(){
 	$('#loginModal').modal('show');
 });
